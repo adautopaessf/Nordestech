df0b210a784baf8d8c2e3b723b864d0b806bcc97
adautopaes <adauto.paessf@gmail.com>
2024-06-10 17:15:33 -0300

    Telas de login, criação de novo usuário e recuperação de senha

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         }
         supportActionBar?.hide()
         Handler().postDelayed({
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
